ef5ab41817647f27460dc22850965e2dc9ed2f1a
lizhigong <306128847@qq.com>
2026-07-30 17:09:34 +0800

    style: fix HCU lint checks

--- python/sglang/srt/speculative/eagle_info.py
+++ python/sglang/srt/speculative/eagle_info.py
@@ -1107,16 +1107,14 @@ class EagleDraftExtendInput(SpecInput):
 
         self.capture_hidden_mode = CaptureHiddenMode.LAST
         self.positions = torch.empty_like(batch.input_ids, dtype=torch.long)
-        self.bonus_tokens = torch.empty_like(
-            self.num_accept_tokens, dtype=torch.int32
-        )
+        self.bonus_tokens = torch.empty_like(self.num_accept_tokens, dtype=torch.int32)
         if self.use_sglang_create_extend_after_decode_spec_info:
             hcu_create_extend_after_decode_spec_info(
-                verified_id = batch.input_ids,
-                seq_lens = batch.seq_lens,
-                accept_lens = self.num_accept_tokens,
-                positions = self.positions,
-                new_verified_id = self.bonus_tokens,
+                verified_id=batch.input_ids,
+                seq_lens=batch.seq_lens,
+                accept_lens=self.num_accept_tokens,
+                positions=self.positions,
+                new_verified_id=self.bonus_tokens,
                 # bs = max(speculative_num_steps + 1, len(batch.seq_lens)),
                 bs=len(batch.seq_lens),
             )
